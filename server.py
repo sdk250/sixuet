5a6a376af60e5a8d04cd765dc77cdffb3a1e8817
sdk250 <520sdk250@gmail.com>
2022-12-02 12:47:25 +0800

    none

--- server.py
+++ server.py
@@ -1,40 +1,41 @@
-from flask import request, Response, Flask
+from flask import request, Response, Flask, render_template
 from re import compile
 from datetime import datetime
 from hashlib import md5
+from time import time
 from json import loads, dumps
 
 app = Flask(__name__)
 
 @app.route(rule = "/")
 def __init__():
-	cookies = open("any.cookies", "ab+")
 	if "Token" in request.cookies.keys():
-		example = Response("Hi, World!")
-		print(compile("^([a-z0-9]+[^= ]) ?= ?(.+)$").findall(cookies.readline().decode("UTF-8")))
-		
-		# example.response = value
+		cookies = open("any.cookies", "rb")
+		example = Response(render_template("__init__.html", title = "Test", content = "Hi, World!"))
+		print(loads(cookies.read().decode("UTF-8")))
+		cookies.close()
 	else:
-		example = Response("Hello, World!")
+		example = Response(render_template("__init__.html", title = "Test", content = "Hello, World"))
 		token = md5(str(datetime.now()).encode("UTF-8")).hexdigest()
 		Response.set_cookie(self = example, key = "Token", value = token, max_age = 60)
-		data = 
+		cookies = open("any.cookies", "rb")
+		save_cookies = loads(cookies.read().decode("UTF-8"))
+		cookies.close()
+		cookies = open("any.cookies", "wb")
+		data = dict(request.args)
+		data["max_age"] = int(time()) + 60
+		save_cookies[token] = data
+		print(save_cookies)
 		cookies.write(
 			bytes(
 				dumps(
-					[
-						{
-							token: dict(request.args)
-						}
-					]
-				) + "\n", encoding = "UTF-8"
+					save_cookies
+				)
+				+ "\n", encoding = "UTF-8"
 			)
 		)
-	# for i in request.args.keys():
-	# 	print(i, request.args[i])
-	# 	print(dict(request.args))
-	cookies.close()
+		cookies.close()
 	return example
 
 if __name__ == "__main__":
-	app.run(host = "0.0.0.0", port = 20800, debug = True)
+	app.run(host = "0.0.0.0", port = 20880, debug = True)
